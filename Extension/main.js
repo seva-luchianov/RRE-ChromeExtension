d853872d1fc968a460bdee730b472a4ef9211874
Seva Luchianov <seva.luchianov@gmail.com>
2017-11-16 00:11:18 -0500

    recommendations now clickable and deletable, still not blacklistable though
    Signed-off-by: Seva Luchianov <seva.luchianov@gmail.com>

--- Extension/main.js
+++ Extension/main.js
@@ -18,25 +18,27 @@ settingsButton.addEventListener('click', function() {
 header.appendChild(settingsButton);
 RREContainer.appendChild(header);
 
+var recommendationsListDIV = document.createElement("lu");
+recommendationsListDIV.setAttribute("id", "recommendations");
+RREContainer.appendChild(recommendationsListDIV);
+
+// Inject into reddit sidebar
+var sideBarDiv = document.getElementsByClassName("side")[0];
+sideBarDiv.insertBefore(RREContainer, sideBarDiv.childNodes[1]);
+
 var xhr = new XMLHttpRequest();
 xhr.open('POST', 'https://localhost:8080/api/subreddits/recommended');
 xhr.onload = function() {
-    var recommendationsListDIV = document.createElement("lu");
+    var recommendationsListDIV = document.getElementById("recommendations");
     if (this.status === 200) {
         var response = JSON.parse(this.response);
         var i;
         for (i in response) {
-            var recommendationDIV = createRecommendationDIV(response[i].subreddit);
-            recommendationsListDIV.appendChild(recommendationDIV);
+            createRecommendationDIV(response[i].subreddit);
         }
     } else {
         recommendationsListDIV.appendChild(document.createTextNode("No Recommendations :("));
     }
-    RREContainer.appendChild(recommendationsListDIV);
-
-    // Inject into reddit sidebar
-    var sideBarDiv = document.getElementsByClassName("side")[0];
-    sideBarDiv.insertBefore(RREContainer, sideBarDiv.childNodes[1]);
 };
 
 xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
@@ -55,8 +57,30 @@ chrome.storage.sync.get([
 });
 
 function createRecommendationDIV(subreddit) {
-    var recommendationDIV = document.createElement("div");
-    recommendationDIV.style.display = "block";
-    recommendationDIV.innerHTML = subreddit;
-    return recommendationDIV;
+    var parentDIV = document.getElementById("recommendations");
+    var id = "recommendations-" + subreddit;
+    console.log("Entry: " + subreddit);
+
+    var entry = document.createElement("div");
+    entry.setAttribute("id", id);
+    entry.setAttribute("class", "recommendation");
+
+    var nameDIV = document.createElement("div");
+    nameDIV.innerHTML = subreddit;
+    nameDIV.style.display = "inline";
+    nameDIV.addEventListener('click', function() {
+        window.location.pathname = this.innerHTML;
+    });
+    entry.appendChild(nameDIV);
+
+    var deleteButton = document.createElement("button");
+    deleteButton.setAttribute("class", "deleteButton");
+    deleteButton.innerHTML = 'X';
+    deleteButton.addEventListener('click', function() {
+        var deleteThis = document.getElementById(this.parentElement.id);
+        deleteThis.parentNode.removeChild(deleteThis);
+        // still need to add to blacklist after removed from normal list.
+    });
+    entry.appendChild(deleteButton);
+    parentDIV.appendChild(entry);
 }
