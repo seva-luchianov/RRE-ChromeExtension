616dfff7e0ce8a71fdd2a1a25c33af7c9f8124e8
Seva Luchianov <seva.luchianov@gmail.com>
2017-11-16 00:54:43 -0500

    fixed add to blacklist when deleteing recommendation

--- Extension/main.js
+++ Extension/main.js
@@ -27,13 +27,12 @@ var sideBarDiv = document.getElementsByClassName("side")[0];
 sideBarDiv.insertBefore(RREContainer, sideBarDiv.childNodes[1]);
 
 var xhr = new XMLHttpRequest();
-xhr.open('POST', 'https://localhost:8080/api/subreddits/recommended');
 xhr.onload = function() {
     var recommendationsListDIV = document.getElementById("recommendations");
     if (this.status === 200) {
         var response = JSON.parse(this.response);
         var i;
-        for (i in response) {
+        for (i = recommendationsListDIV.childElementCount + 1; i < response.length; i++) {
             createRecommendationDIV(response[i].subreddit);
         }
     } else {
@@ -41,20 +40,28 @@ xhr.onload = function() {
     }
 };
 
-xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+refreshRecommendations();
 
-chrome.storage.sync.get([
-    'RRERecommendationLimit',
-    'RRETags',
-    'RREBlackList'
-], function(items) {
-    xhr.send(JSON.stringify({
-        tags: items.RRETags,
-        subscribed: [],
-        blacklisted: items.RREBlackList,
-        maxRecommendations: items.RRERecommendationLimit
-    }));
-});
+function refreshRecommendations(deletedRecommendation) {
+    if (!!deletedRecommendation) {
+        recommendationsListDIV.removeChild(deletedRecommendation);
+    }
+
+    chrome.storage.sync.get([
+        'RRERecommendationLimit',
+        'RRETags',
+        'RREBlackList'
+    ], function(items) {
+        xhr.open('POST', 'https://localhost:8080/api/subreddits/recommended');
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhr.send(JSON.stringify({
+            tags: items.RRETags,
+            subscribed: [],
+            blacklisted: items.RREBlackList,
+            maxRecommendations: items.RRERecommendationLimit
+        }));
+    });
+}
 
 function createRecommendationDIV(subreddit) {
     var parentDIV = document.getElementById("recommendations");
@@ -77,9 +84,19 @@ function createRecommendationDIV(subreddit) {
     deleteButton.setAttribute("class", "deleteButton");
     deleteButton.innerHTML = 'X';
     deleteButton.addEventListener('click', function() {
-        var deleteThis = document.getElementById(this.parentElement.id);
-        deleteThis.parentNode.removeChild(deleteThis);
-        // still need to add to blacklist after removed from normal list.
+        var entryToDelete = document.getElementById(this.parentElement.id);
+        chrome.storage.sync.get([
+            'RREBlackList'
+        ], function(items) {
+            var blacklist = items.RREBlackList;
+            var newBlacklistEntry = entryToDelete.children[0].innerHTML
+            blacklist.push(newBlacklistEntry);
+            chrome.storage.sync.set({
+                RREBlackList: blacklist
+            }, function() {
+                refreshRecommendations(entryToDelete);
+            });
+        });
     });
     entry.appendChild(deleteButton);
     parentDIV.appendChild(entry);
