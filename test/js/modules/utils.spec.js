77f0f35795e46e04b61e5b9b683b6678e7cae0ea
Seva Luchianov <seva.luchianov@gmail.com>
2017-12-08 13:38:50 -0500

    Purged Those premade tests we dont use, started testing utils.js

--- test/js/modules/utils.spec.js
+++ test/js/modules/utils.spec.js
@@ -4,12 +4,29 @@ chai.use(spies);
 const assert = chai.assert;
 const expect = chai.expect;
 var utils = require('../../../src/js/utils');
+var testUtils = require('../utils');
 
 describe('utility functions', () => {
     describe('createListEntry', () => {
-        it('works', () => {
-            // console.log(document.querySelector('body').innerHTML);
-            utils.createListEntry('recommendations');
-        })
+        it('creates the List Entry', () => {
+            var entryValue = testUtils.uuid('message');
+            var deleteFunctionWorks = false;
+            utils.createListEntry('recommendations', entryValue, false, function() {
+                deleteFunctionWorks = true;
+            });
+            var entryDIV = document.getElementById('recommendations-' + entryValue);
+            expect(entryDIV).to.not.be.undefined;
+            if (entryDIV) {
+                var deleteButton = entryDIV.lastElementChild;
+                expect(deleteButton).to.not.be.undefined;
+                if (deleteButton) {
+                    deleteButton.click();
+                    // make sure the click event has time to execute
+                    setTimeout(function() {
+                        expect(deleteFunctionWorks).to.be.true;
+                    }, 10);
+                }
+            }
+        });
     });
 });
